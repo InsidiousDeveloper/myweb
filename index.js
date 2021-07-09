2f798edb1e87b6ae190c80ae09de481d7e11d774
InsidiousDeveloper <asilbekashirov03@gmail.com>
2021-07-09 17:09:55 +0500

    version 1.0.0

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const router = require('./router/index')
+const path = require('path')
 
 const PORT = process.env.PORT || 5000
 const app = express()
@@ -10,6 +11,14 @@ const app = express()
 app.use(express.json({extended: true}))
 app.use(cors())
 app.use('/', router)
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const start = async () => {
     try {
